65452cc6ac7fa379a13c2582036105bc21adc099
KURT.CARRILLO <23101122@usc.edu.ph>
2025-09-15 23:52:19 +0800

    updating, i didnt properly push pag saturday cause of errors in my .git folder

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui"), // 👈 add DaisyUI here
+  ],
+};
+
+export default config;
